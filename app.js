03141783e7558beaa21da216914271b14f568a76
Muskaankamran <muskaankamran494@gmail.com>
2025-11-28 17:32:05 +0500

    practice constructor

---
+++ app.js
@@ -0,0 +1,16 @@
+function Student(name, email, id,) {
+    this.name = name,
+        this.email = email,
+        this.id = id;
+        this.address = {
+            city: 'karachi',
+            country:'Pakistan',
+            area: 'fb area'
+        }
+}
+var student1 = new Student('muskan', 'muskankamran@gmail.com', '1', address);
+var student2 = new Student('sara', 'sara@gmail.com', '2', address)
+console.log(student1);
+console.log(student2);
+
+
