b40f6a4526dc21468d55337f359fc5e59bc56c60
Muskaankamran <muskaankamran494@gmail.com>
2025-11-28 18:00:17 +0500

    practice

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-function Student(name, email, id,) {
-    this.name = name,
-        this.email = email,
-        this.id = id;
-        this.address = {
-            city: 'karachi',
-            country:'Pakistan',
-            area: 'fb area'
-        }
-}
-var student1 = new Student('muskan', 'muskankamran@gmail.com', '1', address);
-var student2 = new Student('sara', 'sara@gmail.com', '2', address)
-console.log(student1);
-console.log(student2);
+// function Student(name, email, id,) {
+//     this.name = name,
+//         this.email = email,
+//         this.id = id;
+//         this.address = {
+//             city: 'karachi',
+//             country:'Pakistan',
+//             area: 'fb area'
+//         }
+// }
+// var student1 = new Student('muskan', 'muskankamran@gmail.com', '1', address);
+// var student2 = new Student('sara', 'sara@gmail.com', '2', address)
+// console.log(student1);
+// console.log(student2);
+
+
+var students = ['student1' , 'student2' , 'student3'];
+var data= localStorage.setItem('neha' , 'sara' , 'fatima');
+console.log(data);
 
 
